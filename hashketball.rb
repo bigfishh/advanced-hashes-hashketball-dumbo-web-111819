2508acfb3f394db74421fc48a3c9f105c50800d0
Annie Zheng <anniezheng.anniee@gmail.com>
2019-11-14 19:42:04 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -188,7 +188,8 @@ def player_stats(player_name)
       if attribute == :players
         data.each do |player|
           if player[:player_name] == player_name
-            new_hash = player.shift
+            player.shift
+            new_hash = player
           end
         end
       end
